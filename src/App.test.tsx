907a5e403efd61a84f4bdef1e1533419c641a295
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-27 20:48:12 +0100

    Add list filter test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { act, render } from "@testing-library/react";
+import { act, fireEvent, render, RenderResult } from "@testing-library/react";
 import { IUser } from "./shared/user.model";
 import UserListView from "./views/UserListView";
 
@@ -26,4 +26,51 @@ test("renders a list", async () => {
   });
 
   expect(component.getByText("Name name @username")).toBeTruthy();
+});
+
+test("filters a list", async () => {
+  const mockJsonPromise = Promise.resolve(mockUsers);
+  const mockFetchPromise = Promise.resolve({
+    ok: true,
+    json: () => mockJsonPromise,
+  });
+  var globalRef: any = global;
+  globalRef.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+  let component: RenderResult;
+
+  await act(async () => {
+    component = render(<UserListView />);
+  });
+
+  const input = component!.getByLabelText("search-input");
+  expect(input).toBeInTheDocument();
+
+  fireEvent.change(input, { target: { value: "23" } });
+  expect(component!.getByText("brak wyników")).toBeTruthy();
+});
+
+test("undos a filter", async () => {
+  const mockJsonPromise = Promise.resolve(mockUsers);
+  const mockFetchPromise = Promise.resolve({
+    ok: true,
+    json: () => mockJsonPromise,
+  });
+  var globalRef: any = global;
+  globalRef.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+  let component: RenderResult;
+
+  await act(async () => {
+    component = render(<UserListView />);
+  });
+
+  const input = component!.getByLabelText("search-input");
+  expect(input).toBeInTheDocument();
+
+  fireEvent.change(input, { target: { value: "23" } });
+  expect(component!.getByText("brak wyników")).toBeTruthy();
+
+  fireEvent.change(input, { target: { value: "" } });
+  expect(() => component!.getByText("brak wyników")).toThrow();
 });
