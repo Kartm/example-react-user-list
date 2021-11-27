9a0d03a82751c128915b27ecded574c75dbd02eb
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-27 18:02:17 +0100

    Add boilerplate to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Styled from "styled-components";
 
-function App() {
+const Container = Styled.div`
+  background: red;
+  width: 100%;
+`;
+const Label = Styled.div`
+  color: white;
+  padding: 20px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Label>Hello World</Label>
+    </Container>
   );
-}
+};
 
 export default App;
