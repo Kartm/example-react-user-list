c4bcaae7bfef886b595d157f6ead0aaedf3f2cc0
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-27 18:30:26 +0100

    Add draft of fetching users from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Styled from "styled-components";
+import debounce from "lodash/debounce";
 
 const Container = Styled.div`
   background: red;
@@ -9,11 +10,61 @@ const Label = Styled.div`
   color: white;
   padding: 20px;
 `;
+const SearchField = Styled.input`
+  padding: 20px;
+`;
+
+//jsonplaceholder.typicode.com/users
+
+interface IUser {
+  id: number;
+  name: string;
+  username: string;
+}
 
 const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setUsers(json);
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, [searchValue]);
+
+  const debounceHandler = useCallback(
+    debounce((value) => {
+      setSearchValue(value);
+    }, 500),
+    []
+  );
+
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+
+      debounceHandler(value);
+    },
+    [debounceHandler]
+  );
+
   return (
     <Container>
-      <Label>Hello World</Label>
+      <SearchField defaultValue={searchValue} onChange={handleInputChange} />
+      {users.map((user, i) => (
+        <div key={user.id}>{`${i + 1}. ${user.name} @${user.username}`}</div>
+      ))}
     </Container>
   );
 };
