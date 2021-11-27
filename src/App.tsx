5d4eca09ca1fc19e2733f71fc7145fbe02466727
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-27 19:10:49 +0100

    Move user list view to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,15 @@
-import React, { useCallback, useEffect, useState } from "react";
 import Styled from "styled-components";
-import debounce from "lodash/debounce";
-import { IUser } from "./user.model";
-import { getUsers } from "./user.api";
-import UserList from "./components/UserList";
+import UserListView from "./views/UserListView";
 
 const Container = Styled.div`
   background: red;
   width: 100%;
 `;
-const Label = Styled.div`
-  color: white;
-  padding: 20px;
-`;
-const SearchField = Styled.input`
-  padding: 20px;
-`;
 
 const App = () => {
-  const [searchValue, setSearchValue] = useState("");
-  const [users, setUsers] = useState<IUser[]>([]);
-  const [filteredUsers, setFilteredUsers] = useState<IUser[]>([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const usersResponse = await getUsers();
-
-      if (usersResponse.error) {
-        setErrorMessage(usersResponse.error);
-        return;
-      }
-
-      setUsers(usersResponse.users);
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    setFilteredUsers(
-      users.filter((user) =>
-        user.name.toLowerCase().includes(searchValue.toLowerCase())
-      )
-    );
-  }, [searchValue, users]);
-
-  const handleInputChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const value = e.target.value;
-
-      setSearchValue(value);
-    },
-    []
-  );
-
   return (
     <Container>
-      <SearchField defaultValue={searchValue} onChange={handleInputChange} />
-      {errorMessage ? (
-        <div>{errorMessage}</div>
-      ) : filteredUsers.length === 0 ? (
-        <div>brak wyników</div>
-      ) : (
-        <UserList users={filteredUsers} />
-      )}
+      <UserListView />
     </Container>
   );
 };
---
+++ src/views/UserListView.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useEffect, useState } from "react";
+import Styled from "styled-components";
+import UserList from "../components/UserList";
+import { getUsers } from "../user.api";
+import { IUser } from "../user.model";
+
+const Container = Styled.div`
+  background: red;
+  width: 100%;
+`;
+
+const SearchField = Styled.input`
+  padding: 20px;
+`;
+
+const UserListView = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<IUser[]>([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const usersResponse = await getUsers();
+
+      if (usersResponse.error) {
+        setErrorMessage(usersResponse.error);
+        return;
+      }
+
+      setUsers(usersResponse.users);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    setFilteredUsers(
+      users.filter((user) =>
+        user.name.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    );
+  }, [searchValue, users]);
+
+  const handleInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+
+      setSearchValue(value);
+    },
+    []
+  );
+
+  return (
+    <Container>
+      <SearchField defaultValue={searchValue} onChange={handleInputChange} />
+      {errorMessage ? (
+        <div>{errorMessage}</div>
+      ) : filteredUsers.length === 0 ? (
+        <div>brak wyników</div>
+      ) : (
+        <UserList users={filteredUsers} />
+      )}
+    </Container>
+  );
+};
+
+export default UserListView;
