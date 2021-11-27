c9b3c5f1eecf7559da5cac8803916886807cd8a7
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-27 20:41:45 +0100

    Add test case that mocks the api call

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, render } from "@testing-library/react";
+import { IUser } from "./shared/user.model";
+import UserListView from "./views/UserListView";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mockUsers: IUser[] = [
+  {
+    id: 1,
+    name: "Name name",
+    username: "username",
+  },
+];
+
+test("renders a list", async () => {
+  const mockJsonPromise = Promise.resolve(mockUsers);
+  const mockFetchPromise = Promise.resolve({
+    ok: true,
+    json: () => mockJsonPromise,
+  });
+  var globalRef: any = global;
+  globalRef.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+  let component: any;
+
+  await act(async () => {
+    component = render(<UserListView />);
+  });
+
+  expect(component.getByText("Name name @username")).toBeTruthy();
 });
