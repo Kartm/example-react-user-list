18ab6e996b331711f467183af9aeba1d46b80012
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-29 19:15:33 +0100

    Merge user list and user row components

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,27 @@
 import Styled from "styled-components";
 import { IUser } from "../shared/user.model";
-import UserRow from "./UserRow";
 
 const Container = Styled.ol`
   padding-inline-start: 8px;
   overflow: auto;
+`;
+
+const UserRowListItem = Styled.li`
+  list-style-position: inside;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;   
+
+  ::marker {
+    content: counter(list-item) ". ";
+    color: grey;
+  }
+
+  a {
+    margin-left: 8px;
+    color: grey;
+    text-decoration: none;
+  }
 `;
 
 interface Props {
@@ -15,7 +32,10 @@ const UserList = ({ users }: Props) => {
   return (
     <Container>
       {users.map((user, i) => (
-        <UserRow user={user} key={i} />
+        <UserRowListItem key={i}>
+          <span>{`${user.name}`}</span>
+          <a href={user.website} target="_blank">{`@${user.username}`}</a>
+        </UserRowListItem>
       ))}
     </Container>
   );
